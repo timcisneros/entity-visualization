559ffb399fb3753ff1eb9b903a99c30ab53cc9ec
tcstria <tcisneros@stria.com>
2023-01-12 14:33:49 -0800

    assigned userpool to value before exporting

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,10 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'us-east-1_eIsaZvDBw',
-  ClientId: 'gu6b8hhks22jkhg5fuo92hh5d',
+    UserPoolId: 'us-east-1_eIsaZvDBw',
+    ClientId: 'gu6b8hhks22jkhg5fuo92hh5d',
 };
 
-export default new CognitoUserPool(poolData);
+const cognitoUserPool = new CognitoUserPool(poolData);
+
+export default cognitoUserPool;
